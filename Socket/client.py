085f47bfee4faf2b4646537e0992e474d6d65fb4
Arnauld ALBERT <arnauld.albert@uha.fr>
2022-11-14 18:45:35 +0100

    modification du serveur et client. Le client peux envoyer plusieurs messages. gestions des erreurs lors des fermetures des clients et serveurs.

--- Socket/client.py
+++ Socket/client.py
@@ -2,14 +2,27 @@ import socket,sys
 
 def client():
    host = ""
-   port = 15000
-   client_socket = socket.socket()
-   client_socket.connect((host, port))
-   message = input("saisir le message -->")
-   client_socket.send(message.encode())
-   data =client_socket.recv(1024).decode()
-   print (f"reponse serveur :{data}")
-   client_socket.close()
+   port = 15002
+   try:
+      client_socket = socket.socket()
+      client_socket.connect((host, port))
+      message = input("saisir votre pseudo")
+      client_socket.send(message.encode())
+      while True:
+         message = input("saisir le message -->")
+         test = client_socket.getsockname()
+         print (test)
+         client_socket.send(message.encode())
+         data =client_socket.recv(1024).decode()
+         print (f"reponse serveur :{data}")
+         if message == "exit":
+            break
+         if client_socket.getsockname():
+            pass
+   except BrokenPipeError:
+      print ("serveur fermé")
+   finally:
+      client_socket.close()
 
 if __name__=="__main__":
     sys.exit(client())
--- Socket/serveur_2.py
+++ Socket/serveur_2.py
@@ -2,30 +2,40 @@ import socket,sys
 import threading
 
 def ecoute(conn, address):
-    data = conn.recv(1024).decode()
-    print(f"(adresse {address[0]} port :{address[1]}) -->  {data}")
-    reply = f"bienvenue sur le serveur de Nono. votre adresse est {address[0]} port :{address[1]}"
-    conn.send(reply.encode())
+    i=1
+    pseudo = conn.recv(1024).decode()
+
+    while True:
+        data = conn.recv(1024).decode()
+        print(f"{pseudo} --> {data} \n")
+        reply = f"reçu {i}"
+        conn.send(reply.encode())
+        i=i+1
+        if str(data).lower() == "exit":
+            break
     conn.close()
 
 def serveur():
-    port = 15000
+    port = 15002
     host = ""
+    try :
+        serveur_socket = socket.socket()
+        serveur_socket.bind((host,port))
+        serveur_socket.listen(5)
+        liste =[]
+        i=0
+        while i < 10:
+            conn, address = serveur_socket.accept()
+            c = threading.Thread(target=ecoute,args=(conn,address))
+            c.start()
+            liste.append(c)
+            i = i+1
 
-    serveur_socket = socket.socket()
-    serveur_socket.bind((host,port))
-    serveur_socket.listen(5)
-    liste =[]
-    i=0
-    while i < 10:
-        conn, address = serveur_socket.accept()
-        c = threading.Thread(target=ecoute,args=(conn,address))
-        c.start()
-        liste.append(c)
-        i = i+1
+    except KeyboardInterrupt:
+        print("fermeture du serveur")
+    finally:
+        serveur_socket.close()
 
-    for c in liste:
-        c.join()
 
 if __name__=="__main__":
     sys.exit(serveur())
